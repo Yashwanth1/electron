3442ebd877dc9d6b4e1fb91e6c0f87c6b08b0743
ydasari <ydasari@qti.qualcomm.com>
2020-10-09 11:36:53 +0530

    1.0.1 update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
